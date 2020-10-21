742614a13c43bfad8b0934019f967783bd8a927c
Matthew Charles Chan <matthew.charles.chan@gmail.com>
2020-10-20 18:36:16 -0700

    add chatmessage form

--- src/Chatroom.js
+++ src/Chatroom.js
@@ -1,23 +1,49 @@
 /* eslint-disable react/prop-types */
-import React from 'react'
+import React, { useState } from 'react'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
+import ChatMessage from './ChatMessage'
 
-export default function Chatroom({ firestore }) {
+export default function Chatroom({ firestore, auth }) {
   const messagesRef = firestore.collection('messages')
   const query = messagesRef.orderBy('createdAt').limit(25)
 
   const [messages] = useCollectionData(query, { idField: 'id' })
+
+  const [formValue, setFormValue] = useState('')
+
+  const sendMessage = async (e) => {
+    e.preventDefault()
+
+    const { uid, photoURL } = auth.currentUser
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL,
+    })
+
+    setFormValue('')
+  }
   // console.log(messagesRef)
+  console.log(auth)
   return (
-    <main>
-      {messages &&
-        messages.map((msg) => {
-          return (
-            <>
-              <h1>{msg.text}</h1>
-            </>
-          )
-        })}
-    </main>
+    <>
+      <main>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+      </main>
+      <form onSubmit={sendMessage}>
+        <input
+          value={formValue}
+          onChange={(e) => setFormValue(e.target.value)}
+          placeholder="send a message"
+        />
+
+        <button type="submit" disabled={!formValue}>
+          🕊️
+        </button>
+      </form>
+    </>
   )
 }
